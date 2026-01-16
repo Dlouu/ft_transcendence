e05b6249a4ec7557afa6f21dbdbf353bd1d37c04
NilsLeVrai <nilslucas@wanadoo.fr>
2026-01-16 15:17:42 +0100

    created base for hashicorp vault

--- srcs/containers/services/auth/app/main.py
+++ srcs/containers/services/auth/app/main.py
@@ -5,7 +5,6 @@ from oauth import oauth
 app = Flask(__name__)
 app.register_blueprint(hashing, url_prefix='/') #registering
 app.register_blueprint(oauth)
-print(" registereddddddddddddddddddddveffwoeghddddddd.")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, use_reloader=True, host='0.0.0.0')
