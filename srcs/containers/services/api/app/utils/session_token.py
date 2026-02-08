13de3306b6559007bf4e590c8ed8dec294cb99d1
Inheert <theoclaereboudt@gmail.com>
2026-02-08 06:52:56 +0100

    API functions and entrypoints docs

--- srcs/containers/services/api/app/utils/session_token.py
+++ srcs/containers/services/api/app/utils/session_token.py
@@ -4,6 +4,14 @@ from app.extensions import cache_token, r
 UNAVAILABLE_MESSAGE = "WARNING: Redis is unavailable, the service might be offline or bad configured in this one."
 
 def does_session_token_exist(key):
+	"""
+	Check if the given token exist in redis.
+
+	param:
+		key: The token you are looking for.
+
+	return: True if found, else False.
+	"""
 	if not r:
 		print(UNAVAILABLE_MESSAGE, flush=True)
 		return None
@@ -11,6 +19,14 @@ def does_session_token_exist(key):
 	return r.exists(f"token:{key}")
 
 def decode_session_token(key):
+	"""
+	Decode the given token WITHOUT handling exception, you need to create this logic when you use this function.
+
+	param:
+		key: The token you want to decode.
+
+	return: None if the token don't exist, else the decoded token (payload).
+	"""
 	if not r:
 		print(UNAVAILABLE_MESSAGE, flush=True)
 
