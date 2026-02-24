ac46d2e3249deda22adaa11e2283ba5bf8164930
Inheert <theoclaereboudt@gmail.com>
2026-02-24 13:00:21 +0100

    logger extra wrapper accept more key, code cleaner

--- srcs/containers/services/auth/app/extensions.py
+++ srcs/containers/services/auth/app/extensions.py
@@ -2,11 +2,18 @@ from flask_sqlalchemy import SQLAlchemy
 from redis.exceptions import ConnectionError
 import redis, os
 
+from app.utils.logger import logger
+
 db = SQLAlchemy()
+
+r = None
 try:
 	r = redis.from_url(os.getenv("REDIS_URL", ""), decode_responses=True)
 	r.ping()
+	logger.info("Redis ready.", extra=logger.extra(target_service="redis"))
 except ValueError as e:
-	print(f"A problem occured when trying to connect to redis from url ({e})", flush=True)
+	logger.fatal(f"A problem occured when trying to connect to redis from url, to avoid any undefined behavior the API will stop. ({e})", extra=logger.extra(target_service="redis"))
+	exit(1)
 except ConnectionError as e:
-	print(f"Failed to ping redis, service will be unavailable ({e})", flush=True)
+	logger.fatal(f"Failed to ping redis, to avoid any undefined behavior the API will stop ({e})", extra=logger.extra(target_service="redis"))
+	exit(1)
