bae8b743b3c8540e9f695026c30ddc7a531b9bf7
Inheert <theoclaereboudt@gmail.com>
2026-02-26 15:38:22 +0100

    nginx init

--- srcs/containers/services/lobby/app/main.py
+++ srcs/containers/services/lobby/app/main.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 from lobby import lobby, socketio
+from flask_socketio import send, disconnect
+
 
 app = Flask(__name__)
 app.register_blueprint(lobby, url_prefix="/")
 
 socketio.init_app(app)
 
-if __name__ == "__main__":
-    socketio.run(app, debug=True, port=5002, host="0.0.0.0", use_reloader=True, allow_unsafe_werkzeug=True)
 
+@socketio.event
+def connect():
+	session_cookie = request.cookies.get("session_id")
+	print("User session:", session_cookie, flush=True)
+	print("Client connected", flush=True)
+	send("Welcome!")
+	disconnect()
+
+@socketio.event
+def disconnect():
+	send("Goodbye!")
+
+@socketio.event
+def message(data):
+	print(data, flush=True)
+
+if __name__ == "__main__":
+	socketio.run(app, debug=True, port=5002, host="0.0.0.0", use_reloader=True, allow_unsafe_werkzeug=True)
