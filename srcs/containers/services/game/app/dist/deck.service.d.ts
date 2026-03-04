6de82e8462d2c460902c6d958f23c3897c21fe52
NilsLeVrai <nilslucas@wanadoo.fr>
2026-03-04 15:24:01 +0100

    deleted dist folder in game

--- srcs/containers/services/game/app/dist/deck.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Card } from "./domain/UnoCard";
-import { Game } from "./domain/UnoGame";
-export declare class DeckService {
-    createDeck(): Card[];
-    discardToDeck(game: Game): void;
-    shuffleDeck(deck: Card[]): Card[];
-    startDeal(game: Game): void;
-}
--- srcs/containers/services/game/app/dist/dto/next-turn.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export interface INextTurn {
-    currentPlayerIndex: number;
-    turnDirection: "CLOCKWISE" | "COUNTER-CLOCKWISE";
-}
-export declare class NextTurnDto implements INextTurn {
-    currentPlayerIndex: number;
-    turnDirection: "CLOCKWISE" | "COUNTER-CLOCKWISE";
-}
