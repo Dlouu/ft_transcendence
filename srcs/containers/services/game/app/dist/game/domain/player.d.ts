4cfeb742ff6cfa6d6454c6219cde5035175f348e
NilsLeVrai <nilslucas@wanadoo.fr>
2026-03-16 16:07:15 +0100

    deleted useless files

--- srcs/containers/services/game/app/dist/game/domain/player.d.ts
+++
@@ -1,9 +0,0 @@
-import { card } from "./card";
-import { Socket } from "socket.io";
-export declare class player {
-    _name: string;
-    _isBot: boolean;
-    _socket: Socket | null;
-    constructor(_name: string, _isBot: boolean, _socket: Socket | null);
-    _hand: card[];
-}
