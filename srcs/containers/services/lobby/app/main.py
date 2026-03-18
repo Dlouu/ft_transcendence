e2b6a9bc493c03db0541f6b18fbcf919d40ca34f
NilsLeVrai <nilslucas@wanadoo.fr>
2026-03-18 14:54:12 +0100

    feature-lobby: added main setup

--- srcs/containers/services/lobby/app/main.py
+++ srcs/containers/services/lobby/app/main.py
@@ -2,4 +2,5 @@ from app import create_app
 from app.core.extensions import socketio
 app = create_app()
 
-
+if __name__ == "__main__":
+	socketio.run(app, debug=True, port=5002, host="0.0.0.0", use_reloader=True, allow_unsafe_werkzeug=True)
