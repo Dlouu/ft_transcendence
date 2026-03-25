b26da1ff24514c18521a1a2e1bc37ff8e76cfaab
Dlouu <135530996+Dlouu@users.noreply.github.com>
2026-03-25 14:26:07 +0100

    Frontend: Login with hook useApi

--- srcs/containers/services/website/app/src/pages/Login.jsx
+++ srcs/containers/services/website/app/src/pages/Login.jsx
@@ -1,9 +1,81 @@
 import { useContext, useState, useRef } from "react";
-import { GameContext } from "../context/GameContext";
-import { AuthContext } from "../context/AuthContext";
-import { Link } from "react-router-dom";
 import { Button, Page, Input, Card } from "../ui";
-import { useNotifications } from "../hooks/useNotifications";
+import { AuthContext } from "../context/AuthContext";
+import { GameContext } from "../context/GameContext";
+import { useApi } from "../hooks/useApi";
+import { Link } from "react-router-dom";
+
+function Login() {
+	const { login } = useContext(AuthContext);
+	const { playerName, setPlayerName } = useContext(GameContext);
+	const { patch, loading } = useApi();
+	const [password, setPassword] = useState("");
+	const passwordRef = useRef(null);
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			await patch("/api/auth/login", {
+				"login_email": playerName,
+				password,
+			});
+
+			await login();
+
+		} catch {
+			return;
+		}
+	};
+
+	return (
+		<Page center>
+			<Card center>
+				<h2 className="text-2xl font-pixelm font-bold mb-6 text-center">
+					LOGIN
+				</h2>
+
+				<form className="w-full text-center" onSubmit={handleSubmit}>
+					<Input
+						placeholder="username or email"
+						value={playerName}
+						onChange={(e) => setPlayerName(e.target.value)}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								e.preventDefault();
+								passwordRef.current?.focus();
+							}
+						}}
+					/>
+
+					<Input
+						placeholder="password"
+						value={password}
+						type="password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+
+					<Button type="submit" disabled={!playerName || !password || loading}>
+						LET'S PLAY
+					</Button>
+					<div className="flex flex-col justify-center items-center">
+						<p className="mt-10">
+							no account ?
+							<Link className="text-purple-300 font-bold p-2" to="/register">
+								register
+							</Link>
+						</p>
+
+					</div>
+				</form>
+			</Card>
+		</Page>
+	);
+}
+
+export default Login;
+
+/*
 
 function Login() {
 	const { login } = useContext(AuthContext);
@@ -47,50 +119,4 @@ function Login() {
 			notify("Login failed", "error");
 		}
 	};
-
-	return (
-		<Page center>
-			<Card center>
-				<h2 className="text-2xl font-pixelm font-bold mb-6 text-center">
-					LOGIN
-				</h2>
-
-				<form className="w-full text-center" onSubmit={handleSubmit}>
-					<Input
-						placeholder="username or email"
-						value={playerName}
-						onChange={(e) => setPlayerName(e.target.value)}
-						onKeyDown={(e) => {
-							if (e.key === "Enter") {
-								e.preventDefault();
-								passwordRef.current?.focus();
-							}
-						}}
-					/>
-
-					<Input
-						placeholder="password"
-						value={password}
-						type="password"
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-
-					<Button onClick={handleSubmit} disabled={!playerName && !password}>
-						LET'S PLAY
-					</Button>
-					<div className="flex flex-col justify-center items-center">
-						<p className="mt-10">
-							no account ?
-							<Link className="text-purple-300 font-bold p-2" to="/register">
-								register
-							</Link>
-						</p>
-
-					</div>
-				</form>
-			</Card>
-		</Page>
-	);
-}
-
-export default Login;
+*/
